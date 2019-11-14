5579285957d7f1ac97c32de75b78e5fea3ce141c
idoco <ido.c.mail@gmail.com>
2019-11-14 19:20:37 +0200

    simple blog test

---
+++ README.md
@@ -0,0 +1,8 @@
+### How to run?
+To install the dependencies issue the below commands in project root directory
+```javascript
+npm install
+``` 
+To run the tests issue the below command
+```javascript
+npm test
---
+++ tests/my-test.js
@@ -0,0 +1,60 @@
+import "chromedriver";
+import { assert } from 'chai';
+import { Builder, By, Key, until } from "selenium-webdriver";
+const server = 'https://loadmill-test-blog.herokuapp.com'
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+describe('Loadmill selenium demo', function () {
+    let driver;
+    this.timeout(50000);
+
+    before(async () => {
+        driver = new Builder().forBrowser('chrome').build();
+    });
+
+    it('Publish a blog post', async function () {
+        const randomPostTitle = 'Test post title ' + Math.floor(Math.random() * 1000000);
+
+        await driver.navigate().to(server + '/ghost/signin/');
+        await driver.wait(until.elementLocated(By.name('identification')));
+
+        // login to blog
+        await driver.findElement(By.name('identification')).sendKeys('a@b.com');
+        await driver.findElement(By.name('password')).sendKeys('Test1234' + Key.RETURN);
+
+        // Create blog post
+        await driver.wait(until.elementLocated(By.xpath('//a[@href="/ghost/editor/"]')));
+        await driver.findElement(By.xpath('//a[@href="/ghost/editor/"]')).click();
+
+        // Set post title and text
+        await driver.wait(until.elementLocated(By.id('entry-title')));
+        await driver.findElement(By.id('entry-title')).sendKeys(randomPostTitle);
+        await driver.findElement(By.xpath('//section[@id="entry-markdown-content"]//textarea')).sendKeys('Test post text');
+
+        await driver.wait(until.elementLocated(By.xpath("//button[text()='Save Draft']"))); // wait for autosave
+        await driver.findElement(By.xpath("//button[text()='Save Draft']")).click(); // save post draft 
+        await driver.wait(until.elementLocated(By.xpath("//button[text()='Save Draft']"))); // wait save to finish
+
+        // Publish post
+        await driver.findElement(By.xpath("//button[.//*[text()='Toggle Settings Menu']]")).click(); // open publish menu
+
+        await driver.wait(until.elementLocated(By.xpath("//a[text()='Publish Now']"))); 
+        await driver.findElement(By.xpath("//a[text()='Publish Now']")).click(); // click publish
+
+        await driver.wait(until.elementLocated(By.xpath("//button[text()='Publish Now']")));
+        await driver.findElement(By.xpath("//button[text()='Publish Now']")).click(); // confirm  publish
+        await driver.wait(until.elementLocated(By.xpath("//button[text()='Update Post']"))); // wait save to finish
+
+        // validate post is published
+        await driver.navigate().to(server);
+        await driver.wait(until.elementLocated(By.xpath("/html/body/div[2]/header/div/div/h1")));
+        let topBlogPost = driver.findElement(By.xpath(`//*[@id='content']/article[1]/header/h2/a[text()='${randomPostTitle}']`));
+        assert.isTrue(await topBlogPost.isDisplayed(), 'Post title match');
+    });
+
+    after(async () => driver.quit());
+});
+
